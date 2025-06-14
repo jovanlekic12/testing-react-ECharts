91092286c0f8dac3a6373b7b3eac0bc47fc5e218
jovanlekic12 <lekicjovan1212@gmail.com>
2025-06-14 23:05:48 +0200

    add chart and pie

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactECharts from "echarts-for-react";
 
 const App: React.FC = () => {
-  const options = {
+  const chartOptions = {
     grid: { top: 8, right: 8, bottom: 24, left: 36 },
     xAxis: {
       type: "category",
@@ -23,11 +23,61 @@ const App: React.FC = () => {
     },
   };
 
+  const pieOptions = {
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      top: "5%",
+      left: "center",
+    },
+    series: [
+      {
+        name: "Access From",
+        type: "pie",
+        radius: ["40%", "70%"],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: "#fff",
+          borderWidth: 2,
+        },
+        label: {
+          show: false,
+          position: "center",
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: "bold",
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: "Search Engine" },
+          { value: 735, name: "Direct" },
+          { value: 580, name: "Email" },
+          { value: 484, name: "Union Ads" },
+          { value: 300, name: "Video Ads" },
+        ],
+      },
+    ],
+  };
+
   return (
-    <ReactECharts
-      option={options}
-      style={{ height: "400px", width: "1500px" }}
-    />
+    <>
+      <ReactECharts
+        option={chartOptions}
+        style={{ height: "400px", width: "1500px" }}
+      />
+      <ReactECharts
+        option={pieOptions}
+        style={{ height: "400px", width: "1500px" }}
+      />
+    </>
   );
 };
 
